bab7b40d38ac79a9f3327b21f84192130553c4b4
philype vinicius <philype20@hotmail.com>
2022-11-30 10:43:33 -0300

    adcionando paginação e modal de confirmação delete

--- django/todo/tasks/static/js/scripts.js
+++ django/todo/tasks/static/js/scripts.js
@@ -1,1 +1,18 @@
-console.log('funciomoi!');
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+});
+
+
