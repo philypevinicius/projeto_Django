c6d2bfca7759791b341dea3acbc34379dc7cd3b8
philype vinicius <philype20@hotmail.com>
2022-12-01 17:23:12 -0300

    Criando uma dashboard para o usuário

--- django/todo/tasks/views.py
+++ django/todo/tasks/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
+import datetime
 
 from .models import Task
 
@@ -12,6 +13,9 @@ def taskList(request):
     
     search = request.GET.get('search')
     filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', update_at__gt=datetime.datetime.now()-datetime.timedelta(days=30), user=request.user).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
     
     if search:
         
@@ -31,7 +35,8 @@ def taskList(request):
         
         tasks = paginator.get_page(page)
     
-    return render(request, 'tasks/list.html', {'tasks':tasks})
+    return render(request, 'tasks/list.html', {'tasks':tasks,
+                                               'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing})
 
 @login_required
 def taskView(request, id):
